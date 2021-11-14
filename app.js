0fb9b802e2049fce95f379e36ea6e732aaa3330d
LiRezende <librusamolin@gmail.com>
2021-11-14 10:13:54 -0300

    Function to delete todos implemented.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const addForm = document.querySelector(".add");
 const list = document.querySelector(".todos");
 const error = document.querySelector(".error");
 
+// Creates the template to display todos
 const createTemplate = (todo) => {
   const html = `
     <li
@@ -13,22 +14,31 @@ const createTemplate = (todo) => {
       "
     >
       <span>${todo}</span>
-      <i class="fas fa-window-close"></i>
+      <i class="fas fa-window-close delete"></i>
     </li>
     `;
   list.innerHTML += html;
 };
-
+// Insert todos on the page
 addForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const todo = addForm.add.value.trim();
+  // Simple validation to check if the form field is empty
   if (todo.length) {
     createTemplate(todo);
     addForm.reset();
+    // If is empty it will display an error message
   } else {
     error.value = "You must type something. Try again.";
     setTimeout(() => {
       addForm.reset();
     }, 2000);
+  }
+});
+
+// Delete todos
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
   }
 });
